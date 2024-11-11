8e7aafa4641361ac9424dd240bc7b25039f526f7
Abdiel Martinez <aabdiel1998@gmail.com>
2024-11-11 15:24:33 -0600

    feat(settings): updated settings files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['unused-imports', '@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -21,5 +21,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'linebreak-style': 0,
+    'unused-imports/no-unused-imports': 'error',
+    'no-console': 'warn',
   },
 };
