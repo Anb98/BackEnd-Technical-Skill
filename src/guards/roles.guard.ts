7ef862b8f8c422b205fed37443b19eaee15a48cd
Abdiel Martinez <aabdiel1998@gmail.com>
2024-11-12 03:01:31 -0600

    refactor(guards): added guard alias path

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -2,7 +2,7 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { Role } from '@prisma/client'
 import { ROLES_KEY } from 'src/decorators/roles.decorator'
-import { Payload } from '../interfaces/payload'
+import { Payload } from '../modules/auth/interfaces/payload'
 
 @Injectable()
 export class RolesGuard implements CanActivate {
--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -2,37 +2,43 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@n
 import { ProductService } from './product.service'
 import { UpdateProductDto } from './dto/update-product.dto'
 import { CreateProductInput } from './inputs/create-product.input'
-import { AuthGuard } from '@modules/auth/guards/auth.guard'
-import { Roles } from 'src/decorators/roles.decorator'
+import { AuthGuard } from '@guards/auth.guard'
 import { Role } from '@prisma/client'
-import { RolesGuard } from '@modules/auth/guards/roles.guard'
+import { RolesGuard } from '@guards/roles.guard'
+import { Roles } from '@decorators/roles.decorator'
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() data: CreateProductInput) {
     return this.productService.create(data, 'xd')
   }
 
-  @UseGuards(AuthGuard, RolesGuard)
-  @Roles(Role.CLIENT)
+  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.productService.findAll()
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.productService.findOne(+id)
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productService.update(+id, updateProductDto)
   }
 
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productService.remove(+id)
